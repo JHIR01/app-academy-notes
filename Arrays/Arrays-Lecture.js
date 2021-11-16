10e0d25dd069896078fb604c7aa67124fe007500
Jacob-Henry Rivere <57297122+JHIR01@users.noreply.github.com>
2021-11-15 19:54:41 -0500

    learning about arrays
    how to make then and how use them in different ways

---
+++ Arrays/Arrays-Lecture.js
@@ -0,0 +1,20 @@
+// var myArray = [];
+// console.log(myArray); // this should just log '(0) []' because there
+// // is nothing in the array as of right now 
+// // method to put an item in the array
+// myArray.push(42);
+// console.log(myArray); // now this will log '(1) [42]' because there
+// // is now an item in the array (index of 1 = "(1)") and 
+// console.log(myArray[0]);
+// myArray.push(100);
+// console.log(myArray);
+// myArray.push("Hello");
+// console.log(myArray);
+// console.log(myArray[2]);
+
+var names = ["Tommy", "Fred", "Mo"];
+console.log(names.length);
+names.push("Jacob");
+console.log(names.length);
+console.log(names);
+console.log(names[names.length -1]);
