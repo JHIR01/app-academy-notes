74ad18484036a35d5434fe798b192f66bec72155
Jacob-Henry Rivere <57297122+JHIR01@users.noreply.github.com>
2021-12-14 00:20:00 -0500

    Just changing a few things
    making some new notes

--- unit-3/Unit-Three-Review/mindPsAndQs-Solution.js
+++ unit-3/Unit-Three-Review/mindPsAndQs-Solution.js
@@ -4,19 +4,23 @@
  */
 
 function mindPsAndQs(str){
-    var  longestStreak = 0;
+    var longestStreak = 0;
     var currentStreak = 0;
     for(var i = 0; i < str.length; i++){
         var char = str[i];
         if(char === 'P' || char === 'Q'){
-            currentStreak++;
-        } else if(currentStreak > longestStreak) {
+            currentStreak++; 
+        } else if(currentStreak > longestStreak) { // if the character is not either 'P' or 'Q' then break the
+            // current steak and see whether or not it is longer than the longest steak.
             longestStreak = currentStreak;
-            currentStreak = 0;
+            currentStreak = 0; // return the streak to 0 because you have found the longest and are now looking
+            // to see if there is another steak in the string that is longer than the newest.
         } else {
-            currentStreak = 0;
+            currentStreak = 0; // if the camparison results in no changes, change the current streak to be 0
+            // and look for a new one that is longer
         }
-        if(i === str.length -1 && currentStreak > longestStreak){
+        if(i === str.length -1 && currentStreak > longestStreak){ // this is for when you reach the end of the 
+            // string and you need to check on whether or not the current is longer than the longest
             longestStreak = currentStreak;
         }
     }
