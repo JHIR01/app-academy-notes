366a25f98eeaecd97c0c904812235b80602c898f
Jacob-Henry Rivere <57297122+JHIR01@users.noreply.github.com>
2021-12-20 13:47:41 -0500

    All thats left is the final assessment
    I feel pretty good now after viewing all those assessments

---
+++ Review/Assessment-Seven/A7/problems/1_number_primes.js
@@ -0,0 +1,24 @@
+/*******************************************************************************
+Write a function numberPrimes(n) that takes in a number, `n`. The function should
+return the number of prime numbers less than or equal to `n`. For example,
+there are 4 prime numbers less that or equal to 10: 2, 3, 5, 7.
+
+Examples:
+
+numberPrimes(10); // => 4
+numberPrimes(12); // => 5
+numberPrimes(20); // => 8
+numberPrimes(100); // => 25
+*******************************************************************************/
+
+function numberPrimes(n) {
+  
+}
+
+console.log(numberPrimes(10)); // => 4
+console.log(numberPrimes(12)); // => 5
+console.log(numberPrimes(20)); // => 8
+console.log(numberPrimes(100)); // => 25
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
+module.exports = numberPrimes;
