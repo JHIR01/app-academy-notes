d10d8e4829d040f747de82793a937b2b0adac111
Jacob-Henry <henryirivere@gmail.com>
2022-02-08 11:25:49 -0500

    just playing around in ruby
    i withdrew from the course a while ago

--- ruby-test.rb
+++ ruby-test.rb
@@ -1,6 +1,8 @@
-class ruby-test
-    def say
-      puts 'Hello terminal!'
-    end
-  end
-  
+puts "Hello terminal!"
+
+def hello_method()
+  puts "This is a message from the method (hello_method):"
+  puts "Hello teminal"
+end
+
+hello_method()
--- test.js
+++ test.js
@@ -1,1 +1,8 @@
 console.log('Hello terminal!');
+
+function hello_function(){
+    console.log("This a message from the function (hello_function()):");
+    console.log("Hello terminal");
+}
+
+hello_function();
